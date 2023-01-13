e3dc40b3d8545a284452b7b274e3ae8c5ad30c9a
LIAUD Corentin <corentin.liaud@orange.fr>
2023-01-13 11:48:13 +0100

    WIP UI
    - Setup yew environment
    - Adds clients listing
    - Create 'common' crate

---
+++ app/src/components/clients.rs
@@ -0,0 +1,54 @@
+use crate::RHOLE_CLIENT;
+use common::Client;
+use log::error;
+use yew::{
+    function_component, html, use_effect_with_deps, use_state, Html, Properties, UseStateHandle,
+};
+
+#[derive(Properties, PartialEq)]
+pub struct ClientProps {}
+
+#[function_component]
+pub fn Clients() -> Html {
+    let all_clients: UseStateHandle<Vec<Client>> = use_state(Vec::new);
+
+    {
+        let all_clients = all_clients.clone();
+
+        use_effect_with_deps(
+            move |_| {
+                wasm_bindgen_futures::spawn_local(async move {
+                    let response = RHOLE_CLIENT.clients().await;
+                    match response {
+                        Ok(clients) => all_clients.set(clients),
+                        Err(e) => {
+                            error!("Error encountered: {e}");
+                            all_clients.set(vec![])
+                        }
+                    }
+                });
+                || ()
+            },
+            (),
+        );
+    }
+
+    let clients_list: Vec<Html> = all_clients
+        .iter()
+        .map(|user| {
+            html! {
+                <div>
+                    <h1>{"Client: "}{user.client_id}</h1>
+                    <p>{"IP address: "} {user.address.clone()}</p>
+                    <p>{"Last seen: "}{user.last_seen}</p>
+                </div>
+            }
+        })
+        .collect();
+
+    html! {
+        <div>
+            {clients_list}
+        </div>
+    }
+}
---
+++ app/src/components/mod.rs
@@ -0,0 +1,9 @@
+mod app;
+mod card;
+mod title;
+mod clients;
+
+pub use clients::Clients;
+pub use card::Card;
+pub use title::Title;
+pub use app::App;
