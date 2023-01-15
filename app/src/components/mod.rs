3f9532a3766007daa9f689802636c04fd226aba1
LIAUD Corentin <corentin.liaud@orange.fr>
2023-01-15 20:12:36 +0100

    Adds API URL retrieval via web_sys:Location object

--- app/src/components/mod.rs
+++ app/src/components/mod.rs
@@ -1,9 +1,9 @@
 mod app;
 mod card;
-mod title;
 mod clients;
+mod title;
 
-pub use clients::Clients;
-pub use card::Card;
-pub use title::Title;
 pub use app::App;
+pub use card::Card;
+pub use clients::Clients;
+pub use title::Title;
