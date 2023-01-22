f71a2a389b64e82d4397bac021fdfef9f96ed90d
LIAUD Corentin <corentin.liaud@orange.fr>
2023-01-22 18:24:37 +0100

    Improves UI

--- rhole-app/src/components/mod.rs
+++ rhole-app/src/components/mod.rs
@@ -1,11 +1,13 @@
 mod app;
 mod blocked_requests;
-mod card;
 mod clients;
+mod grid_component;
+mod grid;
 mod title;
 
+pub use grid_component::GridComponent;
 pub use app::App;
 pub use blocked_requests::BlockedRequests;
-pub use card::Card;
 pub use clients::Clients;
+pub use grid::Grid;
 pub use title::Title;
